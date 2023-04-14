Added Scaling, Fixed Camera, Working on Ground

Data/Components/camera.py
@@ -8,36 +8,40 @@ import ResourceManager as rc
 class Camera(object):
     def __init__(self):
         self.position = pg.math.Vector2()
-        self.size = 1000  # how many units the y axis of the camera is long
+        self.size = rc.CAMERA_ZOOM # how many units the y axis of the camera is long
         self.velocity = pg.math.Vector2()
-
+        self.objects = CameraRenderGroup()
+        self.ground = CameraRenderGroup(False)
 
     def draw_frame(self, surface, objects):
         self.position = np.add(self.position, self.velocity)
 
         objects = objects.get_objects()
 
-        drawableObjects = self.check_within_range(objects)
+        self.check_within_range(objects)
 
-        self.set_objects_world_to_screen_space(drawableObjects)
+        self.set_objects_world_to_screen_space(self.objects)
+        self.set_objects_world_to_screen_space(self.ground)
 
-        drawableObjects.custom_draw(surface)
+        self.ground.custom_draw()
+        self.objects.custom_draw(surface)
+
 
     def set_objects_world_to_screen_space(self, objects):
         for obj in objects:
             obj.screenposition = self.world_to_screen_space(obj.worldposition)
 
     def check_within_range(self, objects):
-        drawableObjects = CameraRenderGroup()
         for obj in objects:
             if not "DRAWABLE" in obj.tags:
                 continue
+            if "GROUND":
+                self.ground.add(obj)
+                continue
             direction = obj.worldposition - self.position
             dist = math.sqrt(direction[0] ** 2 + direction[1] ** 2)
-            if (dist < self.size) or ("ALWAYS_RENDER" in obj.tags):
-                drawableObjects.add(obj)
-
-        return drawableObjects
+            if (dist < self.size * rc.ASPECT_RATIO * 1.3) or ("ALWAYS_RENDER" in obj.tags):
+                self.drawableObjects.add(obj)
 
     def get_keys(self, keys):
         if keys[pg.K_UP]:
@@ -53,26 +57,33 @@ class Camera(object):
 
         pass
 
-    def uv_to_screen_space(self, coords):
-        coords[0] = (coords[0] + 1) / 2.0
-        coords[0] *= rc.SCREEN_SIZE[0]
-        coords[1] = (coords[1] + 1) / 2.0
-        coords[1] = 1 - coords[1]
-        coords[1] *= rc.SCREEN_SIZE[1]
-        return coords
 
     def world_to_screen_space(self, coords):
-        direction = np.array(coords) - self.position
-        direction[0] = direction[0] * (2.0 / ((self.size * rc.SCREEN_SIZE[0]) / rc.SCREEN_SIZE[1]))
-        direction[1] = direction[1] * (2.0 / self.size)
-        screenPos = self.uv_to_screen_space(direction)
-        return screenPos
+        # scaleY        
+        # 0                     
+        # -scaleY / -scaleX     0   scaleX
+        scaleY = self.size
+        scaleX = (self.size / rc.SCREEN_SIZE[1]) * rc.SCREEN_SIZE[0]
+        direction = coords - self.position
+        
+        direction[0] = (direction[0] + scaleX) / (2 * scaleX)
+        direction[0] *= rc.SCREEN_SIZE[0]
+        
+        direction[1] = -(direction[1] - scaleY) / (2 * scaleY)
+        direction[1] *= rc.SCREEN_SIZE[1]
+        return direction
 
 class CameraRenderGroup(pg.sprite.Group):
-    def __init__(self):
+    def __init__(self, sorting = True):
+        self.sorting = sorting
         super().__init__()
         
     def custom_draw(self, surface):
-        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
-            sprite.reset_position(sprite.screenposition)
-            surface.blit(sprite.image, sprite.rect)
+        if self.sorting:
+            for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
+                sprite.reset_position(sprite.screenposition)
+                surface.blit(sprite.image, sprite.rect)
+        else:
+            for sprite in self.sprites():
+                sprite.reset_position(sprite.screenposition)
+                surface.blit(sprite.image, sprite.rect)

Data/Utils/ParentComponents.py
@@ -10,26 +10,16 @@ class BaseSprite(pg.sprite.Sprite):
     def __init__(self, pos, size, *groups):
         pg.sprite.Sprite.__init__(self, *groups)
 
-        self.pixelsize = pg.math.Vector2(self.image.get_size()) #* size
-        leftTopPos = pg.math.Vector2()
-        leftTopPos.x = pos.x - self.pixelsize.x / 2.0
-        leftTopPos.y = pos.y - (self.pixelsize.y / 2.0)
+        size = pg.math.Vector2(size) * resources.UNIT_SCALE
+
+        self.pixelsize = pg.math.Vector2(self.image.get_size())
+        leftTopPosition = pos - (self.pixelsize / 2)
+
+        self.image = pg.transform.scale(self.image, size)
         
-        self.rect = pg.Rect(pos, self.pixelsize)
+        self.rect = pg.Rect(leftTopPosition, self.pixelsize)
         self.exact_position = list(self.rect.topleft)
         self.old_position = self.exact_position[:]
-
-    @property
-    def delta_velocity(self):
-        """
-        Returns the total displacement undergone in a frame. Used for the
-        interpolation of the sprite's location in the draw phase.
-        """
-        return (self.exact_position[0] - self.old_position[0],
-                self.exact_position[1] - self.old_position[1])
-
-    def set_position(self):
-        pass
 
     def reset_position(self, value, attribute="center"):
         """
@@ -52,31 +42,28 @@ class Rigidbody(object):
         self.worldposition = pos
 
     def add_force(self, force):
-        print("Adding force: " + str(force))
-        self.velocity = np.add(self.velocity, force)
-        print("current velocity: " + str(self.velocity))
+        self.velocity += force
 
-    def update(self):
-        dt = pg.time.Clock().get_time()
+    def update(self, dt):
         self.velocity = [self.velocity[x] + self.acceleration[x] for x in range(len(self.acceleration))]
-        self.worldposition += self.velocity
+        self.worldposition += pg.math.Vector2(self.velocity) * (dt / 1000)
 
 
 class GameObject(BaseSprite):
-    def __init__(self, folder, name, pos=pg.math.Vector2(), size=pg.math.Vector2(1,1), *groups):
+    def __init__(self, folder, name, pos, size, *groups):
         self.size = size
-        self.image = resources.GFX[folder][name]
-        
         self.worldposition = pos
-        self.screenposition = pg.math.Vector2()
+
+        self.image = resources.GFX[folder][name]        
 
         BaseSprite.__init__(self, pos, size, *groups)
+
         self.keys = []
 
         # Possible tags are: "ALWAYS_RENDER", "DRAWABLE"
         self.tags = {"DRAWABLE"}
 
-    def update(self, now):
+    def update(self, now, keys, dt):
         pass
 
     def draw(self, surface):