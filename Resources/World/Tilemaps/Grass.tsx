e3500632abf02912da53d5854f0b51f09b75f20f
Borribakbeer <boris.kooiman@gmail.com>
2023-05-15 12:45:30 +0200

    Tilemaps

---
+++ Resources/World/Tilemaps/Grass.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Grass" tilewidth="16" tileheight="16" tilecount="9" columns="3">
+ <image source="../../Graphics/Tilemaps/Grass.png" width="48" height="48"/>
+ <wangsets>
+  <wangset name="Grass" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="3" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="4" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="5" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="6" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="7" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="8" wangid="1,0,0,0,0,0,1,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
