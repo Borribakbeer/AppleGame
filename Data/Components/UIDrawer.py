ab39dd9eeedfc5166d855897b99c8456ff5a0770
Aurora 3 <boris.kooiman@gmail.com>
2023-06-20 12:53:46 +0200

    did a thing

---
+++ Data/Components/UIDrawer.py
@@ -0,0 +1,12 @@
+import pygame as pg
+
+class UIDrawer():
+    def __init__(self):
+        pass
+
+    def draw(self, surface):
+        
+        # Draw how many apples still need to be collected
+        
+        pass
+        
